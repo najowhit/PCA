d40bb070c8a3b1206bef801a2b26f76448412c91
Nathan White <najowhit@iupui.edu>
2017-01-25 23:43:12 -0500

    added basic file loading

--- assign1-najowhit.py
+++ assign1-najowhit.py
@@ -1,5 +1,13 @@
-import numpy as np
+import numpy
+import sys
 
-a = [1,2,3,4,5]
-arr = np.array(a)
-print(arr)
+
+if len(sys.argv) != 2:
+    print("Supply a filename as an argument")
+    exit(1)
+
+dataFile = sys.argv[1]
+
+data = numpy.loadtxt(dataFile, delimiter=",", usecols=(0, 1, 2, 4, 5, 6, 7, 8, 9))
+print(data)
+
