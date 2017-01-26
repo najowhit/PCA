6e58d12be8c7ea391a3f41264fe659ee5e4d3807
Nathan White <najowhit@iupui.edu>
2017-01-25 23:53:40 -0500

    file error detection expanded upon

--- assign1-najowhit.py
+++ assign1-najowhit.py
@@ -8,6 +8,11 @@ if len(sys.argv) != 2:
 
 dataFile = sys.argv[1]
 
-data = numpy.loadtxt(dataFile, delimiter=",", usecols=(0, 1, 2, 4, 5, 6, 7, 8, 9))
-print(data)
+try:
+    data = numpy.loadtxt(dataFile, delimiter=",", usecols=(0, 1, 2, 4, 5, 6, 7, 8, 9))
+    print(data)
+
+except IOError:
+    print("Error loading file")
+    exit(1)
 
