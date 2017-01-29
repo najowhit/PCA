c74b664d58a8324c219913262eda59cebd5eccce
Nathan White <najowhit@iupui.edu>
2017-01-29 18:06:47 -0500

    data centered

--- assign1-najowhit.py
+++ assign1-najowhit.py
@@ -1,18 +1,48 @@
 import numpy
 import sys
 
+'''matrix = numpy.array([[5, 3], [1, 4]])
+#square = numpy.shape(matrix)
 
-if len(sys.argv) != 2:
-    print("Supply a filename as an argument")
-    exit(1)
+oneV = numpy.ones(width)
+#print(oneV)
 
-dataFile = sys.argv[1]
+mean = matrix.mean(axis=0)
+#print(mean)
 
-try:
-    data = numpy.loadtxt(dataFile, delimiter=",", usecols=(0, 1, 2, 4, 5, 6, 7, 8, 9))
-    print(data)
+center = matrix - oneV*mean
+print(center)'''
 
-except IOError:
-    print("Error loading file")
-    exit(1)
+def computeCov(data):
+    #Center the data
+    square = numpy.shape(data)
+    one_vector = numpy.ones(square)
+
+    mean = data.mean(axis=0)
+
+    z_center = data - one_vector*mean
+
+    #Compute covariance
+    #Compare with: np.cov(Z, bias=True, rowvar=False)
+
+def main():
+
+    if len(sys.argv) != 2:
+        print("Supply a filename as an argument")
+        exit(1)
+
+    dataFile = sys.argv[1]
+
+    try:
+        originalData = numpy.loadtxt(dataFile, delimiter=",", usecols=(0, 1, 2, 4, 5, 6, 7, 8, 9))
+
+    except IOError:
+        print("Error loading file")
+        exit(1)
+
+    computeCov(originalData)
+
+
+main()
+
 
