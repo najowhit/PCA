80ca229a54925c1a1250ae6c74331a3d167cb298
Nathan White <najowhit@iupui.edu>
2017-01-30 23:04:44 -0500

    working on part 2

--- assign1-najowhit.py
+++ assign1-najowhit.py
@@ -1,5 +1,7 @@
 import numpy
 import sys
+from numpy import linalg
+
 
 ''''# Center the data
 data = numpy.array([[1,4], [3,8], [7, 2], [6,1], [3,0]])
@@ -39,6 +41,7 @@ print(center)
 
 
 def computeCov(data):
+
     # Center the data
     row, columns = numpy.shape(data)
     one_vector = numpy.ones((row, columns))
@@ -49,14 +52,36 @@ def computeCov(data):
 
     # Compute covariance
     # covariance =  zTz / n
-    my_cov = numpy.dot(z_center.transpose(), z_center) / float(row)
+    my_cov = numpy.dot(z_center.transpose(), z_center) / (row * 1.00)
 
     # Compare covariance with numpy.cov
 
     numpy_cov = numpy.cov(z_center, rowvar=0, bias=1)
 
     cov_equality = numpy.allclose(my_cov, numpy_cov)
+    print('my_cov = numpy_cov')
     print(cov_equality)
+
+
+def dEig(data):
+
+    # Eigenvector that corresponds to largest eigenvalue = largest eigenvector
+
+    numpy_cov = numpy.cov(data, rowvar=0, bias=1)
+    val, vec = linalg.eig(numpy_cov)
+
+    # Eigenvectors are the columns of vec
+    t_vec = vec.transpose()
+
+    # Find largest eigenvalue
+
+    sorted_val = numpy.argsort(val)
+    print(sorted_val)
+
+    # Get the index that corresponds to largest eigenvalues
+    first = sorted_val[len(sorted_val) - 1]
+    second = sorted_val[len(sorted_val) - 2]
+    print(t_vec[first])
 
 
 
@@ -66,16 +91,17 @@ def main():
         print("Supply a filename as an argument")
         exit(1)
 
-    dataFile = sys.argv[1]
+    data_file = sys.argv[1]
 
     try:
-        originalData = numpy.loadtxt(dataFile, delimiter=",", usecols=(0, 1, 2, 4, 5, 6, 7, 8, 9))
+        original_data = numpy.loadtxt(data_file, delimiter=",", usecols=(0, 1, 2, 4, 5, 6, 7, 8, 9))
 
     except IOError:
         print("Error loading file")
         exit(1)
 
-    computeCov(originalData)
+    computeCov(original_data)
+    dEig(original_data)
 
 
 main()
